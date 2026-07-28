69913269fe5fd200b9b6f1a40344db1de5743516
Mitsuaki Kawamura <kawamitsuaki@gmail.com>
2026-07-29 01:25:30 +0900

    最初は次のような責務を持つクラスにします。

--- paser.py
+++ paser.py
@@ -1,2 +1,21 @@
 class QEParser:
-    def parse(self, filename)
+    def __init__(self):
+        self.text        # ファイル全体
+        self.lines       # 行ごとのリスト
+        self.data        # 出力するJSON相当の辞書
+        ...
+
+    def parse(self, filename):
+        ...
+
+    def _parse_program(self):
+        ...
+
+    def _parse_system(self):
+        ...
+
+    def _parse_ionic_steps(self):
+        ...
+
+    def _parse_timing(self):
+        ...
